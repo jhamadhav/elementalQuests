184237e637b38ecf5e35141eb8f7a73a1a8795f3
jhamadhav <jhamadhav28@gmail.com>
2023-04-14 18:19:42 +0530

    game 1 fetch request written

---
+++ public/scripts/game-1.js
@@ -0,0 +1,28 @@
+window.onload = () => {
+
+    let ans = document.getElementById("answer").value
+    document.getElementById("ans-btn").onclick = async () => {
+        let data = {
+            answer: ans
+        }
+        console.log(data);
+        let res = await postData("/checkAnswer", data)
+        console.log(res);
+    }
+}
+
+const postData = async (url = "", data = {}) => {
+    const response = await fetch(url, {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        redirect: "follow",
+        referrerPolicy: "no-referrer",
+        body: JSON.stringify(data)
+    });
+    return response.json();
+}
