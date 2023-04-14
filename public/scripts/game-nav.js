dd5b7dbb192ed8a96866c084ebb8697c8b61c1c6
jhamadhav <jhamadhav28@gmail.com>
2023-04-14 23:55:12 +0530

    instruction and story page turned on before every game

--- public/scripts/game-nav.js
+++ public/scripts/game-nav.js
@@ -1,30 +1,27 @@
-window.onload = () => {
+// pop pages
+let instructionsDiv = document.getElementById("instructions")
+let storyDiv = document.getElementById("story")
 
-    // pop pages
-    let instructionsDiv = document.getElementById("instructions")
-    let storyDiv = document.getElementById("story")
-
-    // btn to close pop page
-    let instructionsCloseBtn = document.getElementById("instructions-btn")
-    instructionsCloseBtn.onclick = () => {
-        closePopPage(instructionsDiv)
-    }
-
-    let storyCloseBtn = document.getElementById("story-btn")
-    storyCloseBtn.onclick = () => {
-        closePopPage(storyDiv)
-    }
-
-    // nav buttons to open pop pages
-    document.getElementById("story-nav").onclick = () => {
-        openPopPage(storyDiv)
-    }
-
-    document.getElementById("instructions-nav").onclick = () => {
-        openPopPage(instructionsDiv)
-    }
-
+// btn to close pop page
+let instructionsCloseBtn = document.getElementById("instructions-btn")
+instructionsCloseBtn.onclick = () => {
+    closePopPage(instructionsDiv)
 }
+
+let storyCloseBtn = document.getElementById("story-btn")
+storyCloseBtn.onclick = () => {
+    closePopPage(storyDiv)
+}
+
+// nav buttons to open pop pages
+document.getElementById("story-nav").onclick = () => {
+    openPopPage(storyDiv)
+}
+
+document.getElementById("instructions-nav").onclick = () => {
+    openPopPage(instructionsDiv)
+}
+
 
 const closePopPage = (div) => {
     div.style.display = "none"
