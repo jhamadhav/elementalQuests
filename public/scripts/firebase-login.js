6ae3a2808575920d3ecb8b446102acabfbdde071
jhamadhav <jhamadhav28@gmail.com>
2023-10-29 14:31:56 +0530

    only authentication elements remaning

--- public/scripts/firebase-login.js
+++ public/scripts/firebase-login.js
@@ -1,284 +1,298 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, sendPasswordResetEmail } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js'
-
+import {
+   getAuth,
+   createUserWithEmailAndPassword,
+   onAuthStateChanged,
+   signInWithEmailAndPassword,
+   signOut,
+   sendPasswordResetEmail,
+} from "https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCqMd47g0ZX0saJRi_1qlH1few24qg_VRk",
-    authDomain: "elemental-quests-firebase.firebaseapp.com",
-    projectId: "elemental-quests-firebase",
-    storageBucket: "elemental-quests-firebase.appspot.com",
-    messagingSenderId: "704041297836",
-    appId: "1:704041297836:web:21a4542ef3f2af6d613c09"
+   apiKey: "AIzaSyCqMd47g0ZX0saJRi_1qlH1few24qg_VRk",
+   authDomain: "elemental-quests-firebase.firebaseapp.com",
+   projectId: "elemental-quests-firebase",
+   storageBucket: "elemental-quests-firebase.appspot.com",
+   messagingSenderId: "704041297836",
+   appId: "1:704041297836:web:21a4542ef3f2af6d613c09",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth()
-let appUser = null
+const auth = getAuth();
+let appUser = null;
 
 window.onload = () => {
+   // according to url param show input boxes
+   let url = new URL(window.location);
+   let val = url.searchParams.get("type");
 
-    // according to url param show input boxes
-    let url = new URL(window.location);
-    let val = url.searchParams.get("type")
+   let loginInp = document.getElementsByClassName("login-inp");
+   let signInp = document.getElementsByClassName("signup-inp");
+   let resetInp = document.getElementsByClassName("reset-inp");
+   let adminInp = document.getElementsByClassName("admin-inp");
 
-    let loginInp = document.getElementsByClassName("login-inp")
-    let signInp = document.getElementsByClassName("signup-inp")
-    let resetInp = document.getElementsByClassName("reset-inp")
-    let adminInp = document.getElementsByClassName("admin-inp")
+   makeDisplayNone(loginInp);
+   makeDisplayNone(signInp);
+   makeDisplayNone(resetInp);
+   makeDisplayNone(adminInp);
 
-    makeDisplayNone(loginInp)
-    makeDisplayNone(signInp)
-    makeDisplayNone(resetInp)
-    makeDisplayNone(adminInp)
+   let temp = loginInp;
+   if (val == "signup") {
+      temp = signInp;
+   } else if (val == "reset") {
+      temp = resetInp;
+   } else if (val == "admin") {
+      temp = adminInp;
+   }
 
-    let temp = loginInp
-    if (val == "signup") {
-        temp = signInp
-    } else if (val == "reset") {
-        temp = resetInp
-    } else if (val == "admin") {
-        temp = adminInp
-    }
+   for (let i = 0; i < temp.length; ++i) {
+      temp[i].style.display = "flex";
+   }
 
-    for (let i = 0; i < temp.length; ++i) {
-        temp[i].style.display = "flex"
-    }
+   document.getElementById("submit-sign-up").onclick = async () => {
+      showBtnLoader();
+      await signUpUser();
+   };
+   document.getElementById("submit-admin-inp").onclick = async () => {
+      showBtnLoader();
+      await loginUser();
+   };
+   document.getElementById("submit-sign-up").onclick = async () => {
+      showBtnLoader();
+      await signUpUser();
+   };
+   document.getElementById("submit-login").onclick = async () => {
+      showBtnLoader();
+      await loginUser();
+   };
+   document.getElementById("submit-logout").onclick = async () => {
+      showBtnLoader();
+      logoutUser();
+   };
+   document.getElementById("submit-reset-password").onclick = async () => {
+      showBtnLoader();
+      resetPassword();
+   };
 
-    document.getElementById("submit-sign-up").onclick = async () => {
-        showBtnLoader()
-        await signUpUser()
-    }
-    document.getElementById("submit-admin-inp").onclick = async () => {
-        showBtnLoader()
-        await loginUser()
-    }
-    document.getElementById("submit-sign-up").onclick = async () => {
-        showBtnLoader()
-        await signUpUser()
-    }
-    document.getElementById("submit-login").onclick = async () => {
-        showBtnLoader()
-        await loginUser()
-    }
-    document.getElementById("submit-logout").onclick = async () => {
-        showBtnLoader()
-        logoutUser()
-    }
-    document.getElementById("submit-reset-password").onclick = async () => {
-        showBtnLoader()
-        resetPassword()
-    }
+   onAuthStateChanged(auth, (user) => {
+      if (user) {
+         // console.log(user);
+         appUser = {};
+         appUser.uid = user.uid;
+         appUser.email = user.email;
 
-    onAuthStateChanged(auth, (user) => {
-        if (user) {
-            // console.log(user);
-            appUser = {}
-            appUser.uid = user.uid
-            appUser.email = user.email
+         user
+            .getIdToken(true)
+            .then((idToken) => {
+               // console.log("id token retrieved")
+               appUser.idToken = idToken;
+               // console.log(idToken);
+               document.cookie = `idToken=${idToken}`;
 
-            user.getIdToken(true).then((idToken) => {
-                // console.log("id token retrieved")
-                appUser.idToken = idToken
-                // console.log(idToken);
-                document.cookie = `idToken=${idToken}`;
-
-                let url = new URL(window.location);
-                let val = url.searchParams.get("type")
-                if (val == "signup") {
-                    iziToast.success({ title: "Redirect", message: "to game" })
-                    window.location = "/game"
-                    return
-                }
-                if (val == "admin") {
-                    iziToast.success({ title: "Redirect", message: "to dashboard" })
-                    window.location = "/admin"
-                    return
-                }
-                hideLoader()
-            }).catch((error) => {
-                hideLoader()
-                console.log("Error while getting login info")
-                console.log(error);
+               let url = new URL(window.location);
+               let val = url.searchParams.get("type");
+               if (val == "signup") {
+                  iziToast.success({ title: "Redirect", message: "to game" });
+                  window.location = "/game";
+                  return;
+               }
+               if (val == "admin") {
+                  iziToast.success({
+                     title: "Redirect",
+                     message: "to dashboard",
+                  });
+                  window.location = "/admin";
+                  return;
+               }
+               hideLoader();
             })
+            .catch((error) => {
+               hideLoader();
+               console.log("Error while getting login info");
+               console.log(error);
+            });
 
-            // console.log(appUser)
+         // console.log(appUser)
 
-            successMsg("logged in")
-        } else {
-            hideLoader()
-            appUser = null
-            console.log("user not available");
-        }
-    });
-}
+         successMsg("logged in");
+      } else {
+         hideLoader();
+         appUser = null;
+         console.log("user not available");
+      }
+   });
+};
 
 const signUpUser = async () => {
-    let userData = {}
-    userData.email = document.getElementById("email").value
-    userData.pass = document.getElementById("password").value
-    userData.confPass = document.getElementById("confirm-password").value
-    console.log(userData);
+   let userData = {};
+   userData.email = document.getElementById("email").value;
+   userData.pass = document.getElementById("password").value;
+   userData.confPass = document.getElementById("confirm-password").value;
+   console.log(userData);
 
+   userData.email = userData.email.trim();
+   userData.pass = userData.pass.trim();
+   userData.confPass = userData.confPass.trim();
+   if (userData.email == "") {
+      errorMsg("Type in your email !");
+      return;
+   }
 
-    userData.email = userData.email.trim()
-    userData.pass = userData.pass.trim()
-    userData.confPass = userData.confPass.trim()
-    if (userData.email == "") {
-        errorMsg("Type in your email !")
-        return
-    }
+   if (userData.pass == "" || userData.confPass == "") {
+      errorMsg("Type in your password !");
+      return;
+   }
 
-    if (userData.pass == "" || userData.confPass == "") {
-        errorMsg("Type in your password !")
-        return
-    }
+   if (userData.pass != userData.confPass) {
+      console.log("enter same password in both fields");
+      errorMsg("Passwords do not match");
+      return;
+   }
 
-    if (userData.pass != userData.confPass) {
-        console.log("enter same password in both fields");
-        errorMsg("Passwords do not match");
-        return
-    }
+   try {
+      let userCredential = await createUserWithEmailAndPassword(
+         auth,
+         userData.email,
+         userData.pass
+      );
 
-    try {
-        let userCredential = await createUserWithEmailAndPassword(auth, userData.email, userData.pass)
+      let user = await userCredential.user;
+      let idToken = user["accessToken"];
+      document.cookie = `idToken=${idToken}`;
 
-        let user = await userCredential.user
-        let idToken = user["accessToken"]
-        document.cookie = `idToken=${idToken}`;
+      successMsg("signed up");
+      successMsg("redirecting to game");
+      window.location = "/game";
 
-        successMsg("signed up")
-        successMsg("redirecting to game")
-        window.location = "/game"
+      console.log("user signed up");
+      console.log(user);
+   } catch (e) {
+      console.log("signup failed");
+      console.log(e);
 
-        console.log("user signed up");
-        console.log(user);
-
-    } catch (e) {
-        console.log("signup failed");
-        console.log(e);
-
-        errorMsg("signup failed");
-    }
-}
+      errorMsg("signup failed");
+   }
+};
 
 const loginUser = async () => {
+   let email = document.getElementById("email").value;
+   let pass = document.getElementById("password").value;
+   if (email == "") {
+      errorMsg("Type in your email !");
+      return;
+   }
+   if (pass == "") {
+      errorMsg("Type in your password !");
+      return;
+   }
+   email = email.trim();
+   pass = pass.trim();
+   try {
+      let userCredential = await signInWithEmailAndPassword(auth, email, pass);
+      let user = userCredential.user;
+      // console.log(user);
+      let idToken = user["accessToken"];
+      document.cookie = `idToken=${idToken}`;
 
-    let email = document.getElementById("email").value
-    let pass = document.getElementById("password").value
-    if (email == "") {
-        errorMsg("Type in your email !")
-        return
-    }
-    if (pass == "") {
-        errorMsg("Type in your password !")
-        return
-    }
-    email = email.trim()
-    pass = pass.trim()
-    try {
-        let userCredential = await signInWithEmailAndPassword(auth, email, pass)
-        let user = userCredential.user
-        // console.log(user);
-        let idToken = user["accessToken"]
-        document.cookie = `idToken=${idToken}`;
+      successMsg("logged in");
+      window.location = "/game";
 
-        successMsg("logged in")
-        window.location = "/game"
+      console.log("user logged in");
+   } catch (e) {
+      // if already logged in move to game
+      if (appUser != null) {
+         // console.log("already logged in");
+         successMsg("Already logged in");
+         successMsg("Redirecting to game");
+         window.location = "/dashboard";
+         return;
+      }
 
-        console.log("user logged in");
-    } catch (e) {
-
-        // if already logged in move to game 
-        if (appUser != null) {
-            // console.log("already logged in");
-            successMsg("Already logged in")
-            successMsg("Redirecting to game")
-            window.location = "/game"
-            return
-        }
-
-        console.log("login failed");
-        errorMsg("login failed");
-        console.log(e);
-    }
-}
+      console.log("login failed");
+      errorMsg("login failed");
+      console.log(e);
+   }
+};
 
 const logoutUser = () => {
-    signOut(auth).then(() => {
-        document.cookie = "idToken=";
-        appUser = null
-        console.log("logged out");
+   signOut(auth)
+      .then(() => {
+         document.cookie = "idToken=";
+         appUser = null;
+         console.log("logged out");
 
-        successMsg("logged out");
-    }).catch((error) => {
-        console.log("error in log out");
-        console.log(error);
+         successMsg("logged out");
+      })
+      .catch((error) => {
+         console.log("error in log out");
+         console.log(error);
 
-        errorMsg("error in log out");
-    });
-}
+         errorMsg("error in log out");
+      });
+};
 
 const resetPassword = () => {
-    let email = document.getElementById("email").value
-    if (email == "") {
-        errorMsg("Type in your email !")
-        return
-    }
-    sendPasswordResetEmail(auth, email)
-        .then(() => {
-            console.log("password reset mail sent")
+   let email = document.getElementById("email").value;
+   if (email == "") {
+      errorMsg("Type in your email !");
+      return;
+   }
+   sendPasswordResetEmail(auth, email)
+      .then(() => {
+         console.log("password reset mail sent");
 
-            successMsg("password reset mail sent")
-        }).catch((error) => {
-            console.log("error while sending password reset mail")
-            console.log(error);
+         successMsg("password reset mail sent");
+      })
+      .catch((error) => {
+         console.log("error while sending password reset mail");
+         console.log(error);
 
-            errorMsg("error while sending password reset mail")
-        });
-}
+         errorMsg("error while sending password reset mail");
+      });
+};
 
 const successMsg = (txt) => {
-    hideBtnLoader()
-    iziToast.success({ title: "Success", message: txt })
-}
+   hideBtnLoader();
+   iziToast.success({ title: "Success", message: txt });
+};
 const errorMsg = (txt) => {
-    hideBtnLoader()
-    iziToast.error({ title: "Error", message: txt })
-}
+   hideBtnLoader();
+   iziToast.error({ title: "Error", message: txt });
+};
 
 const makeDisplayNone = (elem) => {
-    for (let i = 0; i < elem.length; ++i) {
-        elem[i].style.display = "none"
-    }
-}
+   for (let i = 0; i < elem.length; ++i) {
+      elem[i].style.display = "none";
+   }
+};
 
 const showBtnLoader = () => {
-    let btnLoader = document.getElementsByClassName("btn-loader")
-    for (let i = 0; i < btnLoader.length; ++i) {
-        btnLoader[i].style.display = "block"
-    }
+   let btnLoader = document.getElementsByClassName("btn-loader");
+   for (let i = 0; i < btnLoader.length; ++i) {
+      btnLoader[i].style.display = "block";
+   }
 
-    let btns = document.getElementsByTagName("button")
-    for (let i = 0; i < btns.length; ++i) {
-        btns[i].style.color = "transparent"
-    }
-}
+   let btns = document.getElementsByTagName("button");
+   for (let i = 0; i < btns.length; ++i) {
+      btns[i].style.color = "transparent";
+   }
+};
 
 const hideBtnLoader = () => {
-    let btnLoader = document.getElementsByClassName("btn-loader")
-    for (let i = 0; i < btnLoader.length; ++i) {
-        btnLoader[i].style.display = "none"
-    }
+   let btnLoader = document.getElementsByClassName("btn-loader");
+   for (let i = 0; i < btnLoader.length; ++i) {
+      btnLoader[i].style.display = "none";
+   }
 
-    let btns = document.getElementsByTagName("button")
-    for (let i = 0; i < btns.length; ++i) {
-        btns[i].style.color = "#fff"
-    }
-}
+   let btns = document.getElementsByTagName("button");
+   for (let i = 0; i < btns.length; ++i) {
+      btns[i].style.color = "#fff";
+   }
+};
 
 const hideLoader = () => {
-    let loader = document.getElementsByClassName("loader-container")
-    loader[0].style.display = "none"
-}
+   let loader = document.getElementsByClassName("loader-container");
+   loader[0].style.display = "none";
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-var cors = require('cors')
+var cors = require("cors");
 
 // port infos
 const port = process.env.PORT || 8000;
@@ -8,249 +8,36 @@ const port = process.env.PORT || 8000;
 // parser
 app.use(express.static("public"));
 app.use(express.json());
-app.use(cors())
-
+app.use(cors());
 
 // custom modules
-const { checkAuth, hasRole } = require("./middleware/auth")
-const { readDoc, addUserToDB, readGameData, addGameToDB, updateGameDetail, updateGlobalScore, readCollection } = require("./firestoreDB")
-const { gameLogic } = require("./utils/gameSuccessLogic")
+const { checkAuth, hasRole } = require("./middleware/auth");
 
 app.get("/", checkAuth, (request, response) => {
-    response.sendFile(__dirname + "../public/index.html");
+   response.sendFile(__dirname + "../public/index.html");
 });
 
-
-app.get("/game", checkAuth, async (req, res) => {
-
-    // check if user exists in db else add
-    let isUserInDB = await readDoc(req.userData.email)
-    let newUserFlag = false
-    if (isUserInDB == undefined) {
-        addUserToDB(req.userData.email)
-        newUserFlag = true
-    }
-
-    // get current game
-    let userDBdata = await readDoc(req.userData.email)
-    let currentGame = 1
-    try {
-        currentGame = userDBdata["currentGame"]
-    } catch (e) {
-        console.log("Error while accessing current game");
-        console.log(e);
-        res.redirect("/")
-    }
-
-    // if game has ended move to result area
-    if ((newUserFlag == false) && (userDBdata["hasEnded"] == true || currentGame > 5)) {
-        res.sendFile('result.html', { root: './public/gamePages' });
-        return
-    }
-
-    // if game data is not added to user data, add that
-    let len = Object.keys(userDBdata["games"]).length
-    if (len != currentGame) {
-        addGameToDB(userDBdata)
-    }
-
-    // send user th appropriate page 
-    res.sendFile(`game-${currentGame}.html`, { root: './public/gamePages' });
-})
+app.get("/dashboard", checkAuth, async (req, res) => {
+   res.sendFile(`dashboard.html`, { root: "./public" });
+});
 
 // TODO: add check auth middleware
 // checkAuth
 app.get("/admin", checkAuth, async (req, res) => {
-    let cond = hasRole(req, "admin")
-    if (cond) {
-        res.sendFile('admin.html', { root: './public/gamePages' });
-        return
-    }
+   let cond = hasRole(req, "admin");
+   if (cond) {
+      res.sendFile("admin.html", { root: "./public" });
+      return;
+   }
 
-    res.sendFile('403.html', { root: './public' });
-})
+   res.sendFile("403.html", { root: "./public" });
+});
 
-app.post("/admin", checkAuth, async (req, res) => {
-    let cond = hasRole(req, "admin")
-    if (cond) {
-        let allUserData = await readCollection()
-        let globalData = await readGameData("global")
-        let maxData = await readGameData("max-total")
-
-        res.send(JSON.stringify({ allUserData, globalData, maxData }))
-        return
-    }
-
-    res.status(403)
-    res.send(JSON.stringify({ error: 403 }))
-})
-
-app.post("/checkAnswer", checkAuth, async (req, res) => {
-    let data = req.body
-    // console.log(data);
-
-    // get user answer
-    let userAnswer = data["answer"]
-
-    // get complete user data
-    let userDBdata = await readDoc(req.userData.email)
-    let currentGame = userDBdata["currentGame"]
-    // console.log(currentGame)
-
-    // for last stage
-    if (currentGame == 5) {
-        // game has ended now update those data
-        userDBdata["hasEnded"] = true
-        userDBdata["endTime"] = Date.now()
-
-        // calculate total time and update DB
-        let finishTime = userDBdata["endTime"] - userDBdata["startTime"]
-        finishTime = Math.ceil(finishTime / (60 * 1000))
-
-        userDBdata["totalTime"] = finishTime
-    }
-
-    // update current game answer
-    userDBdata["games"][currentGame]["answer"] = userAnswer.toString()
-
-    // read gameData from DB for current game
-    let gameData = await readGameData(currentGame)
-    // console.log(gameData);
-
-    // calculate score and status from game logic
-    let logic = gameLogic[currentGame](gameData, userDBdata, userAnswer)
-
-    // console.log(logic);
-    if (logic["status"] == 1) {
-
-        // if correct answer update score
-        updateGameDetail(gameData, userDBdata, logic)
-
-        // for last stage
-        if (currentGame == 5) {
-            // add user data to global record
-            updateGlobalScore(userDBdata)
-        }
-
-        res.send(JSON.stringify({ status: 1, msg: "correct" }))
-        return
-    }
-
-    // if incorrect answer increment attempt
-    updateGameDetail(gameData, userDBdata, logic)
-    res.send(JSON.stringify({ status: 0, msg: "incorrect" }))
-
-})
-
-app.post("/skipGame", checkAuth, async (req, res) => {
-
-    // get user data from db
-    let userDBdata = await readDoc(req.userData.email)
-    let currentGame = userDBdata["currentGame"]
-    // console.log(currentGame)
-
-    //get current game data from db
-    let gameData = await readGameData(currentGame)
-    // console.log(gameData);
-
-    // since user skipped the level, pass the level with score 0
-    let logic = {
-        status: 1,
-        score: 0
-    }
-
-    // update game detial in user DB
-    updateGameDetail(gameData, userDBdata, logic)
-
-    res.send(JSON.stringify({ status: 1, msg: "correct" }))
-
-})
-
-app.post("/endGame", checkAuth, async (req, res) => {
-
-    // get user data from DB
-    let userDBdata = await readDoc(req.userData.email)
-    let currentGame = userDBdata["currentGame"]
-    // console.log(currentGame)
-
-    // game has ended now update those data
-    userDBdata["hasEnded"] = true
-    userDBdata["endTime"] = Date.now()
-
-    // calculate total time and update DB
-    let finishTime = userDBdata["endTime"] - userDBdata["startTime"]
-    finishTime = Math.ceil(finishTime / (60 * 1000))
-
-    userDBdata["totalTime"] = finishTime
-
-    let gameData = await readGameData(currentGame)
-    // console.log(gameData);
-
-    // since game has been forcefully ended treat current game as skip
-    let logic = {
-        status: 1,
-        score: 0
-    }
-
-    // update user DB
-    updateGameDetail(gameData, userDBdata, logic)
-
-    // add user data to global record
-    updateGlobalScore(userDBdata)
-
-    res.send(JSON.stringify({ status: 1, msg: "correct" }))
-})
-
-app.get("/dead-end-look-up", checkAuth, async (req, res) => {
-    let userDBdata = await readDoc(req.userData.email)
-    let currentGame = userDBdata["currentGame"]
-
-    let gameReq = req.query.game
-    if (gameReq < currentGame && gameReq > 0 && userDBdata["hasEnded"] == false) {
-        // send user th appropriate page 
-        res.sendFile(`game-${gameReq}.html`, { root: './public/gamePages' });
-        return
-    }
-    // send user th appropriate page 
-    res.redirect("/game")
-
-})
-
-app.get("/result", checkAuth, async (req, res) => {
-    let userData = await readDoc(req.userData.email)
-    let globalData = await readGameData("global")
-    let maxData = await readGameData("max-total")
-
-    res.send(JSON.stringify({ userData, globalData, maxData }))
-})
-
-app.post("/leaderboard", checkAuth, async (req, res) => {
-    let allUserData = await readCollection()
-    // for each user extract their email, totalScore and totalTime
-    let data = []
-    for (let i = 0; i < allUserData.length; ++i) {
-        if (allUserData[i]["hasEnded"] == false) continue
-        // console.log(allUserData[i]);
-
-        let email = allUserData[i]["email"].split("@")[0]
-        let totalScore = allUserData[i]["totalScore"]
-        let totalTime = allUserData[i]["totalTime"]
-        data.push([totalScore, totalTime, email])
-    }
-    // console.log(data);
-    // return data
-
-    res.send(JSON.stringify({ data }))
-})
-
-app.get("/leaderboard", checkAuth, async (req, res) => {
-
-    res.sendFile("leaderboard.html", { root: './public/gamePages' });
-})
-
-app.get('*', (req, res) => {
-    res.sendFile('404.html', { root: './public' });
+app.get("*", (req, res) => {
+   res.sendFile("404.html", { root: "./public" });
 });
 
 // listen for requests :)
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () =>
+   console.log(`Example app listening at http://localhost:${port}`)
+);
